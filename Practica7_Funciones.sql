1b1dfa769f26628c38a2c12b9ac3b6fa403fe9a9
Angelito Belén López <jarolbelenlopez@gmail.com>
2026-01-20 00:18:03 -0400

    Practicas de funciones 7

---
+++ Practica7_Funciones.sql
@@ -0,0 +1,11 @@
+set SERVEROUTPUT on;
+
+DECLARE
+    fecha_nacimiento DATE;
+    dia_semana VARCHAR2(100);
+    
+BEGIN
+    fecha_nacimiento := TO_DATE('19/05/2004');
+    dia_semana := TO_CHAR(fecha_nacimiento, 'DAY');
+    DBMS_OUTPUT.PUT_LINE(dia_semana);
+END;
