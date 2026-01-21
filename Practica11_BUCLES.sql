47da86fa8bd8cd6bdaaba149c51e2cff43de2ff5
Angelito Belén López <jarolbelenlopez@gmail.com>
2026-01-21 18:55:41 -0400

    Practica de Frase AL Reves.

--- Practica11_BUCLES.sql
+++ Practica11_BUCLES.sql
@@ -1,16 +1,35 @@
 set SERVEROUTPUT on;
 
 DECLARE
-    num1 NUMBER(30) := 10;
-    num2 NUMBER(30) := 1;
+    num1 NUMBER(30) := 0;
+    --num2 NUMBER(30) := 1;
     resultado NUMBER(30);
+    j NUMBER := 0;
+    --i2 NUMBER := 0;
 
 BEGIN
-    LOOP 
-        resultado := num1 * num2;
-        DBMS_OUTPUT.PUT_LINE(num1 || ' x ' || num2 || ' = ' || resultado);
-        num2 := num2 +1;
-        EXIT WHEN num2 = 13;
-    END LOOP;
+    
+    <<parent>>
+    LOOP    
+       -- num1 := 0;
+        j := j + 1;
+        DBMS_OUTPUT.PUT_LINE('TABLA DEL ' || j);
+        
+               FOR i IN 1..12 LOOP 
+                    resultado := j * i;
+                    DBMS_OUTPUT.PUT_LINE(j || ' x ' || i || ' = ' || resultado);
+                 --   num2 := num2 +1;
+                    --EXIT WHEN num2 = 13;
+                END LOOP;
+               -- num2 := 1;
+         
+        EXIT WHEN j = 10;
+    END LOOP parent;
+    
 END;
+
+
+
+
+
 
